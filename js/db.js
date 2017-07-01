ad9ec4343b07fd12e93f8b35f1213f93b9b03178
ToTea <koniarone445440@gmail.com>
2017-07-01 20:41:54 +0800

    checkbox search

--- js/db.js
+++ js/db.js
@@ -37,6 +37,7 @@ function afterLoad() {
 
     	$("#studentsName").text(student.name);
     	$("#studentsAge").text(student.age);
+    	$("#studentsClass").text(student.class);
     	$("#studentsId").text(student._id);
 
     	$("#studentsInfo").modal('show');
@@ -49,9 +50,11 @@ setTimeout(afterLoad, 500);
 function addData(){
 	var name = $("#newName").val();
 	var age =  $("#newAge").val();
+	var newClass =  $("#newClass").val();
 	var newStudent = {
 	    name: name,
-	    age: age
+	    age: age,
+	    class: newClass
 	};
 	studentCollection.insert(newStudent);
 	studentCollection.save();
@@ -86,6 +89,7 @@ function updateData(){
 
 	$("#updateName").val(student.name);
 	$("#updateAge").val(student.age);
+	$("#updateClass").val(student.class);
 	$("#updateSave").attr("data-id", studentId);
 
 	$("#updateModal").modal('show');
@@ -97,9 +101,11 @@ function updateSave(){
 	var studentId = $(this).attr("data-id");
 	var name = $("#updateName").val();
 	var age =  $("#updateAge").val();
+	var newClass =  $("#updateClass").val();
 	var newStudent = {
 	    name: name,
-	    age: age
+	    age: age,
+	    class: newClass
 	};
 	studentCollection.updateById(studentId, newStudent);
 	studentCollection.save();
@@ -113,9 +119,22 @@ function search(){
 	var gtAge = $("#gtAge").val();
 	console.log(gtAge);
 
+	var selectedClasses = [];
+
+	if( $("#aClassCheckbox").prop("checked") ){
+		selectedClasses.push("A班");
+	}
+	if( $("#bClassCheckbox").prop("checked") ){
+		selectedClasses.push("B班");
+	}
+
+
 	var students = studentCollection.find({
     	age: {
 	        $gt: gtAge/1
+	    },
+	    class: {
+	    	$in: selectedClasses
 	    }
 	});
 	$("#studentsTable").find("tr").remove();
